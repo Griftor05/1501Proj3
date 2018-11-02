c2592535ab21f0c7f3c3a310cb3640989a2713d8
Griftor05 <Griftor05@gmail.com>
2018-11-01 23:14:43 -0400

    So it works for the windows picture, but for nothing else. So we'll have to figure out what the hell's going on there. Maybe cause the codeword sizes?

--- tester.py
+++ tester.py
@@ -5,7 +5,7 @@ os.chdir("src")
 
 for filename in filenamelist:
     newfilename = filename[0:-3] + "lzw"
-    newexpandedname = filename[0:-3] + "(expanded)" + filename[-3:]
+    newexpandedname = filename[0:-4] + "(expanded)" + filename[-4:]
     os.system("java LZWmod - n < ..\\TestFiles\\" + filename + " > ..\\TestFiles\\" + newfilename)
     os.system("java LZWmod + < ..\\TestFiles\\" + newfilename + " > ..\\TestFiles\\" + newexpandedname)
     print("Did " + filename)
